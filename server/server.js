c063f50f764e23fbc96995ded8f046c5d0f73183
Ewan Bauld <ewan.bauld@gmail.com>
2018-09-08 18:54:12 +0100

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('../config/keys_dev').mongoURI;
+
+// Connect to Mongo DB
+mongoose
+    .connect(db)
+    .then(() => console.log('Mongo DB connected'))
+    .catch(err => console.log(err));
+
+// app.use(passport.initialize());
+
+// Passport Config
+// require('../config/passport')(passport);
+
+// Use Routes 
+// app.use('/api/users', users);
+// app.use('/api/vendors', vendors);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LandingPage msg="Welcome to Bolton Kendo Club"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LandingPage from './components/LandingPage.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    LandingPage
   }
 }
 </script>
